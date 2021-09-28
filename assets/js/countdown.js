8cc4bf12dd493278748cc9e118dff2f5e741727c
verkit <verdybangkit@gmail.com>
2021-09-28 22:43:09 +0700

    update countdown

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,5 +1,6 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 10, 2021 00:00:00").getTime();
+// var countDownDate = new Date("Apr 10, 2021 00:00:00").getTime();
+var countDownDate = new Date().now.getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
